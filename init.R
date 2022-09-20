a0a55dcfc34aa93d750d1b075d3175e024adccce
bruno-ponne <bruno.ponne@gmail.com>
2022-09-20 14:50:44 -0300

    update 04

--- init.R
+++ init.R
@@ -2,9 +2,9 @@
 #
 # Example R code to install packages if not already installed
 #
-install.packages("leaflet")
+install.packages("terra")
 
-my_packages = c("raster", "readr", "sp", "tigris", "plotly", "tidyverse", "shiny")
+my_packages = c("leaflet", "raster", "readr", "sp", "tigris", "plotly", "tidyverse", "shiny")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
